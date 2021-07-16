4a182f2b31f109b39df35765c6bf985c004fb05c
Andy Atari <andrewsalteri@gmail.com>
2021-07-16 12:58:08 -0400

    Created a pong ball

--- pong.py
+++ pong.py
@@ -12,7 +12,7 @@ paddle_one = turtle.Turtle()
 paddle_one.speed(0)
 paddle_one.shape("square")
 paddle_one.shapesize(stretch_wid=5, stretch_len=1)
-paddle_one.color("white")
+paddle_one.color("red")
 paddle_one.penup()
 paddle_one.goto(-350, 0)
 
@@ -21,9 +21,18 @@ paddle_one = turtle.Turtle()
 paddle_one.speed(0)
 paddle_one.shape("square")
 paddle_one.shapesize(stretch_wid=5, stretch_len=1)
-paddle_one.color("white")
+paddle_one.color("blue")
 paddle_one.penup()
 paddle_one.goto(350, 0)
+
+# ball
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("square")
+ball.color("white")
+ball.penup()
+ball.goto(0,0)
+
 
 while True:
     window.update() 
