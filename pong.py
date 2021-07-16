3d1f76f97672d16c8aacf16fa861582835e1aab1
Andy Atari <andrewsalteri@gmail.com>
2021-07-16 14:11:41 -0400

    Added movement on y axis for player 1

--- pong.py
+++ pong.py
@@ -17,13 +17,13 @@ paddle_one.penup()
 paddle_one.goto(-350, 0)
 
 # player 2
-paddle_one = turtle.Turtle()
-paddle_one.speed(0)
-paddle_one.shape("square")
-paddle_one.shapesize(stretch_wid=5, stretch_len=1)
-paddle_one.color("blue")
-paddle_one.penup()
-paddle_one.goto(350, 0)
+paddle_two = turtle.Turtle()
+paddle_two.speed(0)
+paddle_two.shape("square")
+paddle_two.shapesize(stretch_wid=5, stretch_len=1)
+paddle_two.color("blue")
+paddle_two.penup()
+paddle_two.goto(350, 0)
 
 # ball
 ball = turtle.Turtle()
@@ -32,6 +32,24 @@ ball.shape("square")
 ball.color("white")
 ball.penup()
 ball.goto(0,0)
+
+def paddle_one_up():
+    y = paddle_one.ycor()
+    y += 20
+    if y >= 250:
+        y = 250
+    paddle_one.sety(y)
+
+def paddle_one_down():
+    y = paddle_one.ycor()
+    y -= 20
+    if y <= -250:
+        y = -250
+    paddle_one.sety(y)
+
+window.listen()
+window.onkeypress(paddle_one_up, "w")
+window.onkeypress(paddle_one_down, "s")
 
 
 while True:
